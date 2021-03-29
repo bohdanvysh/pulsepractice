037e26f547831970b49facff421c65c872911807
Bohdan <blackbobby228@gmail.com>
2021-03-29 20:18:59 +0300

    changed url of items

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prev_arrow.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next_arrow.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/prev_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/next_arrow.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
