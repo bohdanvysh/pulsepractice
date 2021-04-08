360898b81dc00f12519e9badda166df6cd0729f7
Bohdan <blackbobby228@gmail.com>
2021-04-08 22:07:08 +0300

    fixed js file

--- src/js/script.js
+++ src/js/script.js
@@ -23,8 +23,16 @@ $(document).ready(function(){
       $('.catalog-item__link').each(function(i) {
           $(this).on('click', function(e) {
               e.preventDefault();
-              $('.catalog-item__content').toggleClass('catalog-item__content_active');
-              $('.cataog-item__list').toggleClass('catalog-item__list_active');
+              $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+              $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
           })
+      });
+
+      $('.catalog-item__back').each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
       });
   });
