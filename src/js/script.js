330cee24e76ee8de7bf9e11f8ffae91f087c38d2
Bohdan <blackbobby228@gmail.com>
2021-04-13 20:50:58 +0300

    added mask phone validation

--- src/js/script.js
+++ src/js/script.js
@@ -75,3 +75,5 @@ $(document).ready(function(){
 validateForms('#consultation-form');
 validateForms('#consultation form');
 validateForms('#order form');
+
+$('input[name=phone]') .mask("+380 (99) 999-99-99");
